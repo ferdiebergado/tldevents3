bf0fd6af926bc99886a25089d8b3f22ef1f2c24e
ferdiebergado <ferdiebergado@yahoo.com>
2019-07-18 17:45:23 +0800

    Added Mfo Model

---
+++ database/factories/MfoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Mfo;
+use Faker\Generator as Faker;
+
+$factory->define(Mfo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
