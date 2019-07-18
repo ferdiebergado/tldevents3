024244b17020bc643220bf1e80f37cee2717bb81
ferdiebergado <ferdiebergado@yahoo.com>
2019-07-18 17:45:14 +0800

    Added Keystage Model

---
+++ app/Keystage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+// use Illuminate\Database\Eloquent\Model;
+use App\BaseModel;
+
+class Keystage extends BaseModel
+{
+    //
+}
---
+++ database/factories/KeystageFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Keystage;
+use Faker\Generator as Faker;
+
+$factory->define(Keystage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
