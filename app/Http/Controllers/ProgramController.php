81156fbc8fc13cc29d47632f45f98f32b19d6a5f
ferdiebergado <ferdiebergado@yahoo.com>
2019-07-19 13:51:21 +0800

    Test a program view

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -14,7 +14,7 @@ class ProgramController extends Controller
      */
     public function index()
     {
-        //
+        return view('program/programs');
     }
 
     /**
---
+++ resources/views/program/programs.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<p class="lead">
+    Programs
+</p>
+@endsection
